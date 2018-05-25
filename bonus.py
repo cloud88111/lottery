a2fc45f9447e0118632e2385dcd1b160467f5e5e
cloud88111 <robertolivercollins@gmail.com>
2018-05-25 20:48:04 +0100

    Lottery Scripts

--- bonus.py
+++ bonus.py
@@ -17,11 +17,12 @@ Man = {'Name1': 'name1@email.com',
        'Name2': 'name2@email.com'}
 
 strfrom = 'email'
-server = smtplib.SMTP()
+server = smtplib.SMTP('')
+pwd = ''
 
 draw = lott*3
 
-conn = lt.Lottery(strfrom,server,lott,Man,draw)
+conn = lt.Lottery(strfrom,server,lott,Man,draw,strfrom,pwd)
 strto = lt.Lottery.emaillist(conn)
 
 #each player pays 250 to particpate
@@ -30,7 +31,7 @@ prize = len(lott)*250
 table = lt.Lottery.gettable(conn)
 
 message = "Prize is %s Big Ones" %prize
-#lt.Lottery.sendtable(conn,message)
+lt.Lottery.sendtable(conn,table,message)
 
 time.sleep(15)
 
--- lottery.py
+++ lottery.py
@@ -7,17 +7,24 @@ Created on Fri May 25 15:23:58 2018
 
 class Lottery:
     
-    def __init__ (self, strfrom, server, participants, emails, full_list):
+    def __init__ (self, strfrom, server, participants, emails, full_list, username, password):
         self.participants = participants
         self.emails = emails
         self.strfrom = strfrom
         self.server = server
         self.full_list = full_list
+        self.username = username
+        self.password = password
+        self.server.ehlo()
+        self.server.starttls()
+        try: self.server.login(username,password)
+        except: pass
+        self.server.set_debuglevel(1)
         
     #gets list of people to email depending on participants
     def emaillist(self):
-        self.email_list = dict((key,value) for key, value in self.emails.items() if key in self.participants)
-        return list(self.email_list.values())
+        email_list = dict((key,value) for key, value in self.emails.items() if key in self.participants)
+        return list(email_list.values())
     
     def counter(self,list1):
         my_dict = {}
@@ -42,32 +49,24 @@ class Lottery:
         return pd.DataFrame(list(self.counter(self.full_list).items()),columns=['Name','Lives'])
     
     #sends table in an email to the list of participants
-    def sendtable(self,message):
+    def sendtable(self,table,message):
         from email.mime.multipart import MIMEMultipart
         from email.mime.text import MIMEText
-        self.server.ehlo()
-        self.server.starttls()
-        self.server.set_debuglevel(1)
         msgRoot = MIMEMultipart('related')
         msgRoot['Subject'] = message
         msgRoot['From'] = self.strfrom
-        msgRoot['To'] = ','.join(self.emaillist)
+        msgRoot['To'] = ','.join(self.emaillist())
         msgRoot.preamble = 'This is a multi-part message in MIME format.'
         msgAlternative = MIMEMultipart('alternative')
         msgRoot.attach(msgAlternative)
-        TEXT = self.gettable.to_html(index=False)
+        TEXT = table.to_html(index=False)
         msgText = MIMEText(TEXT, 'html')
         msgAlternative.attach(msgText)
-        self.server.sendmail(self.strfrom, self.email_list, msgRoot.as_string())
-        self.server.quit()
+        self.server.sendmail(self.strfrom, self.emaillist(), msgRoot.as_string())
     
     #sends email with the lastest particpant to be removed from the draw    
     def sendmail(self,body):
-        self.server.ehlo()
-        self.server.starttls()
-        self.server.set_debuglevel(1)
-        self.server.sendmail(self.strfrom, self.emaillist, body)
-        self.server.quit()
+        self.server.sendmail(self.strfrom, self.emaillist(), body)
         
     def pullout(self):
         import random
@@ -88,12 +87,11 @@ class Lottery:
                 subj = "Pulled out %s is:" %ordinal(count)
                 BODY = '\r\n'.join(['Subject: %s' % subj,
                 '', message])
-                print(BODY)
-                #self.sendmail(BODY)
+                self.sendmail(BODY)
                 if count%3==0:
                     message = "Lives Update"
-                    print(message, self.gettable)
-                    #self.sendtable(message)
+                    table = self.gettable()
+                    self.sendtable(table,message)
                     time.sleep(15)
                 count += 1
                 time.sleep(15)
@@ -102,6 +100,6 @@ class Lottery:
                 subj = "WE HAVE A WINNER!"
                 BODY = '\r\n'.join(['Subject: %s' % subj,
                 '', message])
-                print(BODY)
-                #self.sendmail(BODY)
+                self.sendmail(BODY)
                 break
+        self.server.quit()
