0743fe1d92a462eea0d9d3e8175628afd4793942
cloud88111 <robert.collins@ig.com>
2018-05-29 18:16:40 +0100

    Add files via upload

--- lottery.py
+++ lottery.py
@@ -8,17 +8,20 @@ Created on Fri May 25 15:23:58 2018
 import pandas as pd
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import random
 import math
 import time
-      
-class Lottery:
+        
+class Lottery(object):
     
-    def __init__ (self, strfrom, server, participants, emails, full_list, username, password):
+    def __init__ (self, strfrom, server, participants, emails, username="", password="",lives=1,last=True):
         self.participants = participants
         self.emails = emails
         self.strfrom = strfrom
         self.server = server
-        self.full_list = full_list
+        self.lives=lives
+        self.full_list = participants*lives
+        self.last=last
         self.username = username
         self.password = password
         self.server.ehlo()
@@ -40,18 +43,24 @@ class Lottery:
         return my_dict
      
     #checks if there is a winner    
-    def checkwinner(self,list1):
+    def checkwinner(self):
         win = 0
-        dict1 = self.counter(list1)
-        for x in dict1:
-            if dict1[x] == 0:
-                win += 1
-            else: win = win
+        if self.last is True:
+            if len(self.full_list) == 1:
+                win+=1
+        else:
+            dict1 = self.counter(self.full_list)
+            for x in dict1:
+                if dict1[x] == 0:
+                    win += 1
         return win
     
     #gets table for display
     def gettable(self):
-        return pd.DataFrame(list(self.counter(self.full_list).items()),columns=['Name','Lives'])
+        table = pd.DataFrame(list(self.counter(self.full_list).items()),columns=['Name','Lives'])
+        if self.lives==1:
+            return table[['Name']]
+        else: return table
     
     #sends table in an email to the list of participants
     def sendtable(self,table,message):
@@ -72,35 +81,48 @@ class Lottery:
         self.server.sendmail(self.strfrom, self.emaillist(), body)
         
     def pullout(self):
-        import random
         d = random.choice(self.full_list)
         self.full_list.remove(d)
         return d
 
-    def run(self):
+    def run(self,email,subject):
         ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4])
         count = 1
         while True:
             d = self.pullout()
-            check = self.checkwinner(self.full_list)
+            check = self.checkwinner()
             if check == 0:
                 message = "%s" %d
                 subj = "Pulled out %s is:" %ordinal(count)
                 BODY = '\r\n'.join(['Subject: %s' % subj,
                 '', message])
                 self.sendmail(BODY)
-                if count%3==0:
-                    message = "Lives Update"
-                    table = self.gettable()
-                    self.sendtable(table,message)
-                    time.sleep(15)
-                count += 1
                 time.sleep(15)
+                if self.lives != 1:
+                    if count%3==0:
+                        message = "Lives Update"
+                        table = self.gettable()
+                        self.sendtable(table,message)
+                        time.sleep(15)
+                count += 1
             else:
-                message = "%s! Don't you just hate them" %d.upper()        
-                subj = "WE HAVE A WINNER!"
-                BODY = '\r\n'.join(['Subject: %s' % subj,
-                '', message])
-                self.sendmail(BODY)
+                if self.lives==1:
+                    message = "%s" %d
+                    subj = "Pulled out last is:"
+                    BODY = '\r\n'.join(['Subject: %s' % subj,
+                    '', message])
+                    self.sendmail(BODY)
+                    time.sleep(5)
+                    message = '%s! ' %d.upper() + email
+                    for values in self.full_list:
+                        message = '%s! ' %values.upper() + email
+                        BODY = '\r\n'.join(['Subject: %s' % subject,
+                        '', message])
+                        self.sendmail(BODY)
+                else:
+                    message = '%s! ' %d.upper() + email
+                    BODY = '\r\n'.join(['Subject: %s' % subject,
+                    '', message])
+                    self.sendmail(BODY)
                 break
         self.server.quit()
